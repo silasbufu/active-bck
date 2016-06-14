43c8555d514551e1bba0a7889b539d2f99f61142
Iuri Calaras <silasbufu@yahoo.com>
2016-06-14 18:16:26 +0300

    14.06.2016
    Working Test Controller returning JSON

--- src/main/java/com/iuri/active_bck/controller/Test.java
+++ src/main/java/com/iuri/active_bck/controller/Test.java
@@ -3,14 +3,21 @@ package com.iuri.active_bck.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.iuri.active_bck.entity.TestEntity;
+
 
 @Controller
 @RequestMapping(value = "rest/test")
 public class Test {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public void test(){
-		System.out.println("test");
+	@RequestMapping(method = RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public TestEntity test(){
+		TestEntity testEntity = new TestEntity();
+		testEntity.setTestString("dragos");
+		return testEntity;
 	}
 	
 }
---
+++ src/main/java/com/iuri/active_bck/entity/TestEntity.java
@@ -0,0 +1,17 @@
+package com.iuri.active_bck.entity;
+
+public class TestEntity {
+
+	public String testString;
+
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+	
+	
+	
+}
