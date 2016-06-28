58dbbb56c7bb05f59ff5c7d8ff4e8763e515c84f
Iuri Calaras <silasbufu@yahoo.com>
2016-06-28 18:23:24 +0300

    28.06.2016
    sequence

--- src/main/java/com/iuri/active_bck/entity/TestEntity.java
+++ src/main/java/com/iuri/active_bck/entity/TestEntity.java
@@ -3,7 +3,9 @@ package com.iuri.active_bck.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -11,7 +13,8 @@ import javax.persistence.Table;
 public class TestEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "auto_gen")
+	@SequenceGenerator(name = "auto_gen", sequenceName = "test_table_test_id_seq")
 	@Column(name = "test_id")
 	private int testId;
 	@Column(name = "test_string")
